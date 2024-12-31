2a5a01073c3f81b7149b9dfc24dd121403d87e02
The Creator <fleegangstudios@gmail.com>
2024-12-31 03:50:00 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// Example: Display an alert when the "More Info" button is clicked
+document.querySelectorAll('.more-info').forEach(button => {
+    button.addEventListener('click', function() {
+        alert('More information about the NFT.');
+    });
+});
+
+window.onload = function() {
+    var ageConfirmed = confirm("You must be 18 years old or older!");
+    if (!ageConfirmed) {
+        window.location.href = "https://www.google.com";
+    }
+};
+
+function submitAnswer() {
+    var userInput = document.getElementById('userInput').value;
+    alert("Your favorite strain is: " + userInput);
+}
+
